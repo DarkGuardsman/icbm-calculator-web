ac9415b874cb5b7050a286c33907301b83b6ef3f
Robin Seifert <rseifert.phone@gmail.com>
2024-10-22 09:30:34 -0400

    refactor: moving tnt blast to it's own file

---
+++ src/funcs/sims/TNTBlast.tsx
@@ -0,0 +1,77 @@
+import {DebugDotData, DebugLineData} from "../../graph/GraphRender";
+
+export default function tntBlast(cx:number, cz:number,
+                  size: number,
+                  normalize: boolean,
+                  tiles: number[][],
+                  setTile: (x: number, y: number, tileId: number) => void,
+                  addDot: (dot: DebugDotData) => void,
+                  addLine: (line: DebugLineData) => void
+) {
+    addDot({
+        x: cx, y: cz,
+        size: 0.1,
+        color: 'blue'
+    });
+
+    const raysPerAxis = 16;
+    for (let xs = 0; xs < raysPerAxis; ++xs)
+    {
+        //for (int ys = 0; ys < this.raysPerAxis; ++ys)
+        //final int ys = 0;
+        for (let zs = 0; zs < raysPerAxis; ++zs)
+        {
+            if (xs === 0 || xs === raysPerAxis - 1 || /*ys == 0 || ys == raysPerAxis - 1 ||*/ zs === 0 || zs === raysPerAxis - 1)
+            {
+                //Step calculation, between -1 to 1 creating edge slices of a cube
+                let xStep = xs / (raysPerAxis - 1.0) * 2.0 - 1.0;
+                //double yStep = ys / (raysPerAxis - 1.0F) * 2.0F - 1.0F;
+                let zStep = zs / (raysPerAxis - 1.0) * 2.0 - 1.0;
+
+                //Distance
+                const magnitude = Math.sqrt(xStep * xStep + /*yStep * yStep +*/ zStep * zStep);
+
+                //normalize, takes it from a box shape to a circle shape
+                if (normalize)
+                {
+                    xStep /= magnitude;
+                    //yStep /= diagonalDistance;
+                    zStep /= magnitude;
+                }
+
+                //Get energy
+                let radialEnergy = size; //* (0.7F + random.nextFloat() * 0.6F);
+
+                //Get starting point for ray
+                let x = cx;
+                //double y = this.location.y();
+                let z = cz;
+
+                //final Color lineColor = Utils.randomColor();
+
+                for (let step = 0.3; radialEnergy > 0.0; radialEnergy -= step * 0.75)
+                {
+                    addDot({
+                        x,
+                        y: z,
+                        size: 0.1,
+                        color: 'blue'
+                    });
+                    addLine({
+                        startX: x,
+                        startY: z,
+                        endX: x + xStep * step,
+                        endY: z + zStep * step,
+                        size: 0.05,
+                        color: 'red'
+                    });
+
+                    //Iterate location
+                    x += xStep * step;
+                    //y += yStep * step;
+                    z += zStep * step;
+                }
+            }
+        }
+    }
+}
--- src/tools/selector/simulation/SimulationSelector.tsx
+++ src/tools/selector/simulation/SimulationSelector.tsx
@@ -3,6 +3,7 @@ import Select from "react-select";
 import {useState} from "react";
 import {TILE_AIR, TILE_SET} from "../../../common/Tiles";
 import {DebugDotData, DebugLineData} from "../../../graph/GraphRender";
+import tntBlast from "../../../funcs/sims/TNTBlast";
 
 interface TestTypeEntry {
     id: string;
@@ -39,7 +40,7 @@ export default function SimulationSelector({tiles, setTile, addDot, addLine, onR
 
         onRun();
         if(testToRun.id === "minecraft:tnt") {
-            tntBlast(16, 16, 10, false, tiles, setTile, addDot, addLine);
+            tntBlast(16, 16, 6, true, tiles, setTile, addDot, addLine);
         }
         else if (testToRun.id === "random:fill") {
             for (let y = 0; y < tiles.length; y++) {
@@ -73,80 +74,4 @@ export default function SimulationSelector({tiles, setTile, addDot, addLine, onR
             </div>
         </div>
     )
-}
-
-function tntBlast(cx:number, cz:number,
-                  size: number,
-                  normalize: boolean,
-                  tiles: number[][],
-                  setTile: (x: number, y: number, tileId: number) => void,
-                  addDot: (dot: DebugDotData) => void,
-                  addLine: (line: DebugLineData) => void
-) {
-    addDot({
-        x: cx, y: cz,
-        size: 0.1,
-        color: 'blue'
-    });
-
-    const raysPerAxis = 16;
-    for (let xs = 0; xs < raysPerAxis; ++xs)
-    {
-        //for (int ys = 0; ys < this.raysPerAxis; ++ys)
-        //final int ys = 0;
-        for (let zs = 0; zs < raysPerAxis; ++zs)
-        {
-            if (xs === 0 || xs === raysPerAxis - 1 || /*ys == 0 || ys == raysPerAxis - 1 ||*/ zs === 0 || zs === raysPerAxis - 1)
-            {
-                //Step calculation, between -1 to 1 creating edge slices of a cube
-                let xStep = xs / (raysPerAxis - 1.0) * 2.0 - 1.0;
-                //double yStep = ys / (raysPerAxis - 1.0F) * 2.0F - 1.0F;
-                let zStep = zs / (raysPerAxis - 1.0) * 2.0 - 1.0;
-
-                //Distance
-                const magnitude = Math.sqrt(xStep * xStep + /*yStep * yStep +*/ zStep * zStep);
-
-                //normalize, takes it from a box shape to a circle shape
-                if (normalize)
-                {
-                    xStep /= magnitude;
-                    //yStep /= diagonalDistance;
-                    zStep /= magnitude;
-                }
-
-                //Get energy
-                let radialEnergy = size; //* (0.7F + random.nextFloat() * 0.6F);
-
-                //Get starting point for ray
-                let x = cx;
-                //double y = this.location.y();
-                let z = cz;
-
-                //final Color lineColor = Utils.randomColor();
-
-                for (let step = 0.3; radialEnergy > 0.0; radialEnergy -= step * 0.75)
-                {
-                    addDot({
-                        x,
-                        y: z,
-                        size: 0.1,
-                        color: 'blue'
-                    });
-                    addLine({
-                        startX: x,
-                        startY: z,
-                        endX: x + xStep * step,
-                        endY: z + zStep * step,
-                        size: 0.1,
-                        color: 'red'
-                    });
-
-                    //Iterate location
-                    x += xStep * step;
-                    //y += yStep * step;
-                    z += zStep * step;
-                }
-            }
-        }
-    }
 }
